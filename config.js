cab351807aec589e6f5a7b09bc6ef0f2ddf681e9
Dragnir98 <lo7459451@gmail.com>
2025-05-07 22:20:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -31,17 +31,17 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~zcYW1YzB#o3DZRoS-O0ePenwvX63P42VSX-4t6Flsp7eig7dWFqE",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~OIBjjKwJ#hQy4bqjDtIc0wtp4K6kSIkEY8Er1chCQNC2cauSXr34",
     CAPTION: process.env.CAPTION || "POWERED BY 🧛‍♂️MɌX🩸 ĐȺɌꝀ🐦‍🔥 𝔇ℜ𝔄𝔊𝔑ℑℜ👀🧛🏻‍♂️",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
-    MODE: process.env.MODE || "public",
+    MODE: process.env.MODE || "private",
     AUTO_VOICE: process.env.AUTO_VOICE || "false",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Awais-star-a11y/TESTING-REPO/raw/refs/heads/main/IMG-20250505-WA0055.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE IM LA REINE VAIANA 😊♻️",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE IM MRX😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || "'",
@@ -51,13 +51,13 @@ module.exports = {
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
     AUTO_REACT: process.env.AUTO_REACT || "false",
     HEART_REACT: process.env.HEART_REACT || "false",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "50947542823",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "50932844372",
     OWNER_NAME: process.env.OWNER_NAME || "➺🧛‍♂️MɌX🩸 ĐȺɌꝀ🐦‍🔥 𝔇ℜ𝔄𝔊𝔑ℑℜ👀🧛🏻‍♂️ɳƭ_VAIANA࿐",
     READ_CMD: process.env.READ_CMD || "true",
     BOT_NAME: process.env.BOT_NAME || "➺🧛‍♂️MɌX🩸 ĐȺɌꝀ🐦‍🔥 𝔇ℜ𝔄𝔊𝔑ℑℜ👀🧛🏻‍♂️",
-    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү Vaiana`",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү MR AFRIKEN`",
     STATUS_REACT: process.env.STATUS_REACT || "true",
-    ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
+    ANTI_VV: process.env.ANTI_VV || "false",// true for anti once view 
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
